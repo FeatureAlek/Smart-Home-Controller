53dc1aec658101f887a36c07fb6b0e5942f3aa73
Faustas <faustas.alekna@gmail.com>
2026-07-01 10:55:09 +0300

    task1 completion

---
+++ src/main/java/com/bootcamp/smarthome/exception/InvalidValueException.java
@@ -0,0 +1,10 @@
+package com.bootcamp.smarthome.exception;
+
+public class InvalidValueException extends HomeAutomationException{
+
+    public InvalidValueException(String field, Object value, String constraint){
+
+    }
+
+
+}
